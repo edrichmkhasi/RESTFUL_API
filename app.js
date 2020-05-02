c0670d2bff986a2d740f1cd4f71b79b57fc91f26
edrichmkhasi <edrich@thedigitalacademy.co.za>
2020-05-02 03:56:58 +0200

    error handling on app.js

--- app.js
+++ app.js
@@ -3,20 +3,38 @@
 const express = require('express');
 // spin up express fuction
 const app = express();
+const morgan = require('morgan');
 
-const products = require('./api/routes/products');
-//const orders = require('./api/routes/orders');
+// display requests on the console using morgan
+app.use(morgan('dev'));
 
 app.use('/products', require('./api/routes/products'));
 app.use('/orders', require('./api/routes/orders'));
 
 // middleware to handle and pass incoming requests
+// app.use((req, res, next) => {
+// // status 200 for good request
+//     res.status(200).json(
+//       {message: 'Its working'}
+//     );
+// });
+
+
 app.use((req, res, next) => {
-// status 200 for good request
-    res.status(200).json(
-      {message: 'Its working'}
-    );
-});
+  const error = new Error('Not Found');
+  error.status = 404;
+  next(error);
+})
+
+// for database related errors
+app.use((error, req, res, next) => {
+  res.status(error.status|| 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  })
+})
 
 // export to use on other files
 module.exports = app;
